0bae7a5791972ca68a4f753e91bcd52afdfc78d8
pdeveau1 <pdeveau@bu.edu>
2022-03-23 14:59:55 -0400

    Code before reseting and restarting

--- pdeveau_hw5_1.m
+++ pdeveau_hw5_1.m
@@ -1,11 +1,21 @@
-[faces] = load_faces();
-%image #120 in the data set
-face_120 = faces(120,:);
-mean_face = mean(faces,1);
-figure();
-subplot(1,2,1);
-imshow(uint8(reshape(face_120, [112,92])));
-title('Image #120 in the Dataset');
-subplot(1,2,2);
-imshow(uint8(reshape(mean_face, [112,92])));
-title('Mean Face of the Dataset');
+%%%a
+X = [1 1 0 2;3 7 5 5];
+[d,n] = size(X);
+mu_x = (1/n) * sum(X,2)
+one_n = ones(n,1);
+X_tilde = X - mu_x * one_n';
+Sx = (1/n) .* ((X - mu_x) * (X - mu_x)');
+Sx = (1/n) * X_tilde * X_tilde'
+[V,D] = eig(Sx)
+[coeff, score, latent, tsquared, explained] = pca(X)
+
+%%%b
+r = [-1 2 -1 0 2]'
+s = [1 -1 -1 1 1]'
+X = [3*r,r,-1*r,-3*r,3*s,2*s,s,-6*s]
+[d,n] = size(X);
+mu_x = (1/n) * sum(X,2)
+one_n = ones(n,1);
+X_tilde = X - mu_x * one_n'
+Sx = (1/n) .* ((X - mu_x) * (X - mu_x)')
+Sx = (1/n) * X_tilde * X_tilde'
